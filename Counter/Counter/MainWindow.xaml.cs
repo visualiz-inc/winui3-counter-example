d7a893274d8bb8c51d6dd6723f7710dd5e08fad4
rempei-okada <renpei.okada@outlook.com>
2021-03-04 00:04:43 +0900

    テスト監視アプリ

--- Counter/Counter/MainWindow.xaml.cs
+++ Counter/Counter/MainWindow.xaml.cs
@@ -7,52 +7,22 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Counter
 {
-    public class Bindable : INotifyPropertyChanged
-    {
-        int count = 0;
-        string fibText = "N = 0, Fib = 0";
-
-        public int Count
-        {
-            get => count;
-            set
-            {
-                count = value;
-                this.NotyfyPropertyChanged();
-            }
-        }
-
-        public string FibText
-        {
-            get => this.fibText;
-            set
-            {
-                this.fibText = value;
-                this.NotyfyPropertyChanged();
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void NotyfyPropertyChanged([CallerMemberName] string name = "")
-        {
-            this.PropertyChanged?.Invoke(this, new(name));
-        }
-    }
-
     /// <summary>
     /// バインド可能なプロパティ
     /// </summary>
@@ -89,45 +59,84 @@ namespace Counter
     /// </summary>
     public sealed partial class MainWindow : Window
     {
-        private Bindable Bindable { get; } = new();
-        private State<int> Counter = new(1);
-        private State<string> Text = new("");
-        private int fibN = 0;
+        // 最大行数
+        const int MaxLineCount = 1000;
+
+        State<string> Command { get; } = new("ipconfig");
+        State<string> Args { get; } = new("-all");
+        State<ObservableCollection<string>> Logs { get; } = new(new() { "ログ" });
 
         public MainWindow()
         {
             this.InitializeComponent();
         }
 
-        private void HandleCounter1(object sender, RoutedEventArgs e)
+        private async void ExecuteCommand(object sender, RoutedEventArgs e)
         {
-            this.Bindable.Count++;
-        }
-
-        private void HandleCounter2(object sender, RoutedEventArgs e)
-        {
-            this.Counter.Value++;
-        }
-
-        private void HandleFib(object sender, RoutedEventArgs e)
-        {
-            this.fibN++;
-            this.Bindable.FibText = $"N = {this.fibN}, Fib = {CalcFib(this.fibN)}";
-        }
-
-        /// <summary>
-        /// フィボナッチ数を生成します
-        /// </summary>
-        /// <param name="n"></param>
-        /// <returns></returns>
-        private static long CalcFib(int n)
-        {
-            return n switch
+            try
             {
-                0 => 0,
-                1 => 1,
-                _ => CalcFib(n - 1) + CalcFib(n - 2)
-            };
+                using var process = new Process();
+
+                process.StartInfo.FileName = this.Command.Value;
+                process.StartInfo.Arguments = this.Args.Value;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+
+                process.OutputDataReceived += async (_, data) =>
+                {
+                    if (data.Data is not null and string text)
+                    {
+                        // UIスレッドで実行
+                        this.DispatcherQueue.TryEnqueue(() =>
+                        {
+                            this.Logs.Value.Add(text);
+                            // 最大行数を超えたら先頭を削除
+                            if (this.Logs.Value.Count > MaxLineCount)
+                                this.Logs.Value.RemoveAt(0);
+
+                        });
+                    }
+                };
+
+                // 開始
+                process.Start();
+
+                // 標準出力の読み込みを開始
+                process.BeginOutputReadLine();
+
+                // 終了するまで待機
+                await process.WaitForExitAsync();
+
+                // 終了コード
+                var code = process.ExitCode;
+
+                // スタックオーバーフローで終了したかどうか
+                var isStackOverflow = process.ExitCode == unchecked((int)0xC00000FD);
+
+                // UIスレッドで実行
+                this.DispatcherQueue.TryEnqueue(async () =>
+                {
+                    await new ContentDialog()
+                    {
+                        Title = $"終了コード : {code}",
+                        XamlRoot = this.Content.XamlRoot,
+                        CloseButtonText = "Ok"
+                    }.ShowAsync();
+                });
+            }
+            catch (Exception ex)
+            {
+                // UIスレッドで実行
+                this.DispatcherQueue.TryEnqueue(async () =>
+                {
+                    await new ContentDialog()
+                    {
+                        Title = ex.Message,
+                        XamlRoot = this.Content.XamlRoot,
+                        CloseButtonText = "Ok"
+                    }.ShowAsync();
+                });
+            }
         }
     }
 }
